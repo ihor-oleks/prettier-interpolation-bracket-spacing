7919bf69fe5169c0de0dd59e79877cb3e6f0f239
Ihor Oleksandrov <ihor.oleks@pm.me>
2024-08-02 20:03:53 +0300

    Chore: Initialize the repository

---
+++ README.md
@@ -0,0 +1,30 @@
+# Prettier Interpolation Bracket Spacing
+
+A [Prettier](https://prettier.io/) plugin to respect the bracket spacing option in interpolations.
+
+
+## Install
+
+```sh
+npm install --save-dev prettier-interpolation-bracket-spacing
+```
+
+```sh
+pnpm add --save-dev prettier-interpolation-bracket-spacing
+```
+
+```sh
+yarn add --dev prettier-interpolation-bracket-spacing
+```
+
+
+## Usage
+
+```json
+{
+  "tabWidth": 2,
+  "trailingComma": "none",
+  "bracketSpacing": false,
+  "plugins": ["prettier-interpolation-bracket-spacing"]
+}
+```
---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import {AstPath, Doc, format, type Parser, type ParserOptions, type Plugin, Printer} from "prettier";
+import {parsers as angularParsers} from "prettier/plugins/angular";
+import {parsers as typescriptParsers} from "prettier/plugins/typescript";
+
+const getPlugins = (language: string, options: ParserOptions): Plugin[] => {
+  const plugins = options.plugins.filter((plugin) => typeof plugin !== "string") as Plugin[];
+  return plugins.filter((plugin) => plugin.languages?.some((lang) => lang.name === language));
+};
+
+export const printers: {[astFormat: string]: Printer} = {
+  "interpolation-bracket-spacing-ast": {
+    print: (path: AstPath): Doc => {
+      if (path.node?.type === "interpolation-bracket-spacing") {
+        return path.node.body;
+      }
+      throw new Error(`Unknown node type: ${path.node?.type}`);
+    }
+  }
+};
+
+export const parsers: {[parserName: string]: Parser} = {
+  angular: {
+    ...angularParsers.__ng_interpolation,
+    parse: async (text: string, options: ParserOptions) => {
+      const content = await format(text, {...options, plugins: getPlugins("angular", options), endOfLine: "lf"});
+      const body = options.bracketSpacing ? content : content.replaceAll("{{ ", "{{").replaceAll(" }}", "}}");
+      return {
+        type: "interpolation-bracket-spacing",
+        body
+      };
+    },
+    astFormat: "interpolation-bracket-spacing-ast"
+  },
+  typescript: {
+    ...typescriptParsers.typescript,
+    parse: async (text: string, options: ParserOptions) => {
+      const content = await format(text, {...options, plugins: getPlugins("typescript", options), endOfLine: "lf"});
+      const body = options.bracketSpacing ? content : content.replaceAll("{{ ", "{{").replaceAll(" }}", "}}");
+      return {
+        type: "interpolation-bracket-spacing",
+        body
+      };
+    },
+    astFormat: "interpolation-bracket-spacing-ast"
+  }
+};
